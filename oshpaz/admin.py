5fd76519eeefb96677e61f60bb72762720a26a9e
Samir <samirabdurahimov219@gmail.com>
2024-11-06 17:22:06 +0500

    feat(models)

--- oshpaz/admin.py
+++ oshpaz/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from oshpaz.models import  User,Certificate,Achievement,Profession
+from oshpaz import models  
 # Register your models here.
 
-admin.site.register(User)
-admin.site.register(Achievement)
-admin.site.register(Profession)
-admin.site.register(Certificate)
+admin.site.register(models.User)
+admin.site.register(models.Achievement)
+admin.site.register(models.Profession)
+admin.site.register(models.Certificate)
+admin.site.register(models.Product)
+admin.site.register(models.Category)
+admin.site.register(models.Scientist)
+admin.site.register(models.HealthyLife)
+admin.site.register(models.PersonalPrescription)
+admin.site.register(models.Myerrors)
+admin.site.register(models.Announcement)
--- oshpaz/models.py
+++ oshpaz/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 class Profession(models.Model):
     name = models.CharField(max_length=255)
@@ -19,6 +20,11 @@ class Achievement(models.Model):
 
     def __str__(self):
         return f"Achievement {self.id}"
+    
+class Portfolio(models.Model):
+    portfolio = models.FileField()
+
+
 
 
 class User(models.Model):
@@ -51,3 +57,122 @@ class User(models.Model):
 
     def __str__(self):
         return self.full_name
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(BaseModel):
+    category_name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.category_name)
+
+        super(Category, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.category_name
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+
+class Product(models.Model):
+    class RatingChoices(models.IntegerChoices):
+        ZERO = 0
+        ONE = 1
+        TWO = 2
+        THREE = 3
+        FOUR = 4
+        FIVE = 5
+
+    product_name = models.CharField(max_length=255, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    price = models.IntegerField(null=True, blank=True)
+    quantity = models.IntegerField(null=True, blank=True)
+    rating = models.IntegerField(choices=RatingChoices.choices, default=RatingChoices.ZERO.value, null=True, blank=True)
+    discount = models.IntegerField(default=0)
+    slug = models.SlugField(null=True, blank=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, related_name='images')
+    category = models.ForeignKey(Category,on_delete=models.CASCADE, related_name='categories')
+    
+    @property
+    def discount_price(self):
+        if self.discount > 0 :
+            return self.price * (1 - self.discount / 100)
+        
+        return self.price
+    
+
+    @property
+    def pay_mont(self):
+        return self.price / 12
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.product_name)
+
+        super(Product, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.product_name
+
+class OrderMenu(models.Model):
+    pass
+    # product = models.ForeignKey(Product , on_delete=models.CASCADE , related_name='oredermenus')
+
+class Scientist(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(max_length=255)
+    position = models.CharField(max_length=255)
+    desc = models.CharField(max_length=255)
+
+class HealthyLife(models.Model):
+    retsepts = models.CharField(max_length=255)
+    photo = models.FileField()
+    categores = models.ForeignKey(Category , on_delete=models.CASCADE , related_name='healthylifes')
+
+
+class Comment(BaseModel):
+    class RatingChoices(models.IntegerChoices):
+        ZERO = 0
+        ONE = 1
+        TWO = 2
+        THREE = 3
+        FOUR = 4
+        FIVE = 5
+
+    message = models.TextField()
+    rating = models.IntegerField(choices=RatingChoices.choices, default=RatingChoices.ZERO.value, null=True, blank=True)
+    file = models.FileField(upload_to='comments/', null=True, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class PersonalPrescription(models.Model):
+    name  = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    image = models.ImageField()
+    description = models.CharField(max_length=255)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    users_like = models.ManyToManyField(User, related_name='likes', blank=True, db_table='users_like')
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='personalprescriptions' )
+
+class Myerrors(models.Model):
+    title = models.CharField(max_length=255)
+    my_life = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    should_be_done = models.CharField(max_length=255)
+    image = models.ImageField()
+    users_like = models.ManyToManyField(User,blank=True , related_name='my_error_liked' )
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='myerrors' )
+
+class Announcement(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField()
+    
